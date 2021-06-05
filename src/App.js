178078d596c42c97e65f2e32639dd3b848cb9cea
prithvikolachena <65934613+prithvikolachena@users.noreply.github.com>
2021-06-05 12:03:58 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  /*return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +22,19 @@ function App() {
         </a>
       </header>
     </div>
+  );*/
+
+  const[people, setPeople] = useState(data);
+
+  return(
+
+        <main>
+            <section className='container'>
+                <h3>{people.length} birthdays today</h3>
+                <List people={people} />
+                <button onClick = {() => setPeople([])}>clear all </button>
+            </section>
+        </main>
   );
 }
 
